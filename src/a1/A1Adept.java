68afd51505fb939a2667151a65fd346d449390fb
wangshuguang <wangshuguang@gebiniwediannao.wireless-1x.unc.edu>
2019-09-01 15:32:02 -0400

    A1

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -6,6 +6,61 @@ public class A1Adept {
 
 	public static void main(String[] args) {
 		
-		
-	}
-}
+		  Scanner scan = new Scanner(System.in);
+		  int a = scan.nextInt();
+		  String[] objectname = new String[a];
+		  double[] price = new double[a];
+		  for(int i = 0; i < a; i++) {
+		   objectname[i] = scan.next();
+		   price[i] = scan.nextDouble();
+		  }
+		  //b is the number of ppl
+		  int b = scan.nextInt();
+		  String[] firstname = new String[b];
+		  String[] lastname = new String[b];
+		  double[] cost = new double[b];
+		  for(int j = 0; j < b; j++) {
+		   firstname[j] = scan.next();
+		   lastname[j] = scan.next();
+		   int items = scan.nextInt();
+		   for(int k = 0; k < items; k++) {
+		    int quantity = scan.nextInt();
+		    String thisitem = scan.next();
+		    for(int h = 0; h < a; h++) {
+		     
+		     if(thisitem.equals(objectname[h])) {
+		      cost[j] += quantity * price[h];
+		     }
+		    }
+		   }
+		  }
+		  int indexmax = 0;
+		  double maxnumber = 0.0;
+		  for (int x = 0; x < b; x++) {
+		   if(cost[x]> maxnumber) {
+		    indexmax = x;
+		    maxnumber = cost[x];
+		   }
+		  }
+		  int indexmin = 0;
+		  double minnumber = cost[0];
+		  for (int y = 0; y < b; y++) {
+		   if(cost[y]< minnumber ) {
+		    indexmin = y;
+		    minnumber = cost[y];
+		   }
+		  }
+		  double sum = 0;
+		  for (int z = 0; z < b; z++) {
+		   sum += cost[z];
+		  }
+		  double average = sum / b;
+		  scan.close();
+		  System.out.println("Biggest: "+ firstname[indexmax] + " " 
+		  + lastname[indexmax] + " "+ "(" + String.format("%.2f", maxnumber) + ")");
+		  System.out.println("Smallest: "+ firstname[indexmin] + " " 
+		  + lastname[indexmin] + " "+ "(" + String.format("%.2f", minnumber) + ")");
+		  System.out.println("Average" + ": " + String.format("%.2f", average));
+		  // Your code follows here.
+		  }
+		 }
--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -7,8 +7,49 @@ public class A1Jedi {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
 		
+		  int a = scan.nextInt();
+		  String[] objectname = new String[a];
+		  double[] price = new double[a];
+		  int[]objectList = new int[a];
+		  for(int i = 0; i < a; i++) {
+		   objectname[i] = scan.next();
+		   price[i] = scan.nextDouble();
+		  }
+		  int peoplenumber = scan.nextInt();
+		  int[] ppl_buying = new int[a];
+		  
+		  //processing every people
+		  for(int j = 0; j < peoplenumber; j++) {
+		   scan.next();
+		   scan.next();
+		   int items = scan.nextInt();
+		   boolean[] check = new boolean[a];
+		   for (int k = 0; k < items; k++) {
+		    int quantity = scan.nextInt();
+		    String name = scan.next();
+		    for (int l = 0; l < a; l++) {
+		     if (name.equals(objectname[l])) {
+		      objectList[l]+= quantity;
+		      check[l] = true;
+		     }
+		    }
+		    
+		   } 
+		   for(int m = 0; m < a; m++) {
+		    if(check[m] == true) {
+		     ppl_buying[m]++;
+		    }
+		  }
+		  
+		  
+		  }
+		  for(int i = 0; i < a; i++) {
+		   if(ppl_buying[i] == 0) {
+		    System.out.println("No customers bought " + objectname[i]);
+		   } else {
+		   System.out.println(ppl_buying[i] + " customers bought " + objectList[i] + " " + objectname[i]);
+		  }
+		 }
+		}	
 	}
-}
